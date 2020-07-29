66330d1f007f6757a37046bad3048a4caae3cff1
Israel Yance <israelyance@gmail.com>
2020-07-28 20:51:10 -0500

    09-console

---
+++ modulos/console.js
@@ -0,0 +1,67 @@
+// console.log('Algo')
+
+// console.error('Algo')
+
+// console.warn('Algo')
+
+
+// Tabla
+//////
+// var tabla = [
+//     {
+//         a: 1,
+//         b: 'Z'
+//     },
+//     {
+//         a: 2,
+//         b: 'Otra'
+//     }
+// ]
+
+// console.table(tabla)
+
+
+// Group
+//////
+// console.group('Conversación')
+// console.log('Hola')
+// console.group('Bla')
+// console.log('Blablabla')
+// console.log('Blablabla')
+// console.log('Blablabla')
+// console.groupEnd('Bla)
+// console.log('Adios')
+// console.groupEnd('Conversación')
+
+// console.log('Otra cosa de otra función')
+
+/////
+// function funcion1() {
+//     console.group('función 1')
+//     console.log('Esto es de la función 1')
+//     console.log('Esto también')
+//     console.log('Esto también')
+//     console.log('Esto también')
+//     funcion2()
+//     console.log('Hemos vuelto a la 1')
+//     console.groupEnd('función 1')
+// }
+
+// function funcion2() {
+//     console.group('función 2')
+//     console.log('Ahora estamos en la función 2')
+//     console.groupEnd('función 2')
+// }
+
+// console.log('Empezamos')
+// funcion1()
+
+
+// Count
+//////
+console.count('veces')
+console.count('veces')
+console.count('veces')
+console.countReset('veces')
+console.count('veces')
+console.count('veces')
