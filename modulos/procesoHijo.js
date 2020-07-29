8ca493b24a0238c790cbd7f93490ee9668a24902
Israel Yance <israelyance@gmail.com>
2020-07-29 10:09:45 -0500

    11-proceso-hijo

---
+++ modulos/procesoHijo.js
@@ -0,0 +1,26 @@
+const { exec, spawn } = require('child_process')
+
+// exec('ls -la', (err, stdout, stderr) => {
+//     if (err) {
+//         console.error(err)
+//         return false
+//     }
+//     console.log(stdout)
+// })
+
+
+let proceso = spawn('ls', ['-la'])
+
+console.log(proceso.pid)
+console.log(proceso.connected)
+
+proceso.stdout.on('data', function(dato) {
+    console.log('¿Está muerto?')
+    console.log(process.killed)
+    console.log(dato.toString())
+})
+
+proceso.on('exit', function() {
+    console.log('El proceso terminó')
+    console.log(proceso.killed)
+})
