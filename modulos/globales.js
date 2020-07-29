b8f2a5dcc8b098f8dfaf0275213efd88b2e872e1
Israel Yance <israelyance@gmail.com>
2020-07-28 19:55:49 -0500

    07-globals

---
+++ modulos/globales.js
@@ -0,0 +1,32 @@
+
+// Global
+// console.log(global)
+
+// setInterval
+// let i = 0
+
+// let intervalo = setInterval(function() {
+//     console.log('Hola')
+//     if (i === 3) {
+//         clearInterval(intervalo)
+//     }
+//     i++
+// }, 1000)
+
+
+// setImmediate
+// setImmediate(function() {
+//     console.log('Hola')
+// })
+
+
+// require()
+
+
+// console.log(process)
+// console.log(__dirname)
+// console.log(__filename)
+
+
+global.miVariable = 'elValor'
+console.log(miVariable)
